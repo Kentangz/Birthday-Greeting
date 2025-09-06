07d5e10bc86607944b0bf825b6191a996cfc9166
Kentangz <adhitsholawat12@gmail.com>
2025-09-06 10:35:06 +0700

    feat : Add immersive space background

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import { Canvas } from '@react-three/fiber';
-import { Stars, OrbitControls } from '@react-three/drei';
+import { Stars, OrbitControls } from '@react-three/drei'; 
 import { EffectComposer, Bloom } from '@react-three/postprocessing';
 import ShootingStar from './ShootingStar';
 import SolarSystem from './SolarSystem';
+import SpaceBackground from './SpaceBackground';
 
 function Scene() {
   return (
     <Canvas camera={{ position: [0, 15, 25], fov: 75 }}>
-      <color attach="background" args={['black']} />
+      {/* <color attach="background" args={['black']} /> */}
+      <Stars saturation={false} count={100} speed={3} />
+
+      <SpaceBackground />
+      
       <ambientLight intensity={0.1} />
-      <Stars saturation={false} count={500} speed={0.5} />
       
       <ShootingStar />
       
---
+++ src/components/SpaceBackground.jsx
@@ -0,0 +1,21 @@
+import { useEffect } from 'react';
+import { useLoader, useThree } from '@react-three/fiber';
+import { TextureLoader, EquirectangularReflectionMapping } from 'three';
+
+function SpaceBackground() {
+  const { scene } = useThree();
+  const texture = useLoader(TextureLoader, '/textures/galaxy_background.png');
+
+  useEffect(() => {
+    texture.mapping = EquirectangularReflectionMapping;
+    
+    scene.background = texture;
+  
+    scene.environment = texture;
+
+  }, [texture, scene]);
+
+  return null;
+}
+
+export default SpaceBackground;
